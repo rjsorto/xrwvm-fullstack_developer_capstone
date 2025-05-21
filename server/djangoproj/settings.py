ff338839efafea9897d8873bb2445fe492a80a39
Roberto Sorto <roberto.sorto@ondev.one>
2025-05-21 11:17:49 -0600

    module 5

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,10 +29,15 @@ SECRET_KEY =\
 DEBUG = True
 
 # ALLOWED_HOSTS=['localhost','https://roberto1-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-ALLOWED_HOSTS=['localhost','https://roberto1-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = [
+    'localhost',
+    'https://roberto1-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.'
+    + 'proxy.cognitiveclass.ai'
+]
 
 # CSRF_TRUSTED_ORIGINS=['https://roberto1-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS=['https://roberto1-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = ['https://roberto1-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.'
++ 'proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -97,7 +102,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.'
+        + 'UserAttributeSimilarityValidator',
     },
     {
         'NAME':
@@ -146,4 +152,3 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build'),
     os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
-
