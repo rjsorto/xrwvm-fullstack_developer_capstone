3a0f4594473d52a43ffbac40a45b52100668b5fc
Roberto Sorto <roberto.sorto@ondev.one>
2025-05-21 11:53:14 -0600

    module 5 3rd review

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -16,7 +16,8 @@ from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
 
 from .models import CarMake, CarModel
-from .restapis import get_request, analyze_review_sentiments, post_review
+# from .restapis import get_request, analyze_review_sentiments, post_review
+from .restapis import get_request, analyze_review_sentiments
 
 
 # Get an instance of a logger
@@ -87,11 +88,11 @@ def get_dealer_details(request, dealer_id):
 
 def add_review(request):
     if (request.user.is_anonymous is False):
-        data = json.loads(request.body)
+        # data = json.loads(request.body)
         try:
             # response = post_review(data)
             return JsonResponse({"status": 200})
-        except Exception as err:
+        except Exception:
             return JsonResponse({
                 "status": 401,
                 "message": "Error in posting review"
